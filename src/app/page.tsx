2ce08d93198d7c1c82c02aee3f6ee916b90b5d82
Kevin <110005210+Kevinh0o@users.noreply.github.com>
2023-10-28 18:23:43 -0300

    feat/best-practices-code-design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,11 @@
-import Navbar from "./components/navbar";
-import Card from "./components/card";
+import Navbar from "../components/navbar";
+import Card from "../components/card";
 
 export default function Home() {
   
   return (
     <>
-    <main>
       <Navbar title="S.M.Place"></Navbar>
-    </main>
     </>
   );
 };
