3d9be6270a2b23bd0e66d708894bdb7cc7e06eb6
Cecato <gustavocecato010620@gmail.com>
2023-10-29 00:17:18 -0300

    setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,89 @@
 import Card from "../components/card";
+import Bar from "../components/banner";
 
 export default function Home() {
   
   return (
     <>
       <main>      
-        <div>
-          <Card
-              img="/images/default.png"
-              alt="img"
-              title="Instagram"
-              text="Curtidas"
-              price="A partir de 2$"
-            />        
+        <div className="w-full h-screen bg-gray-200 flex flex-col p-5 items-center">
+          <div className="max-w-screen-xl h-screen border border-gray-300 bg-white rounded-md shadow-md w-full">
+            <Bar>
+              <Card
+                  imgSrc="/images/instagram.png"
+                  alt="img"
+                  title="Instagram"
+                  text="Curtidas"
+                  price="A partir de 2$"
+                /> 
+                <Card
+                  imgSrc="/images/tiktok.png"
+                  alt="img"
+                  title="Instagram"
+                  text="Curtidas"
+                  price="A partir de 2$"
+                />
+                <Card
+                  imgSrc="/images/instagram.png"
+                  alt="img"
+                  title="Instagram"
+                  text="Curtidas"
+                  price="A partir de 2$"
+                />  
+                <Card
+                  imgSrc="/images/instagram.png"
+                  alt="img"
+                  title="Instagram"
+                  text="Curtidas"
+                  price="A partir de 2$"
+                />
+                <Card
+                  imgSrc="/images/instagram.png"
+                  alt="img"
+                  title="Instagram"
+                  text="Curtidas"
+                  price="A partir de 2$"
+                />    
+            </Bar>
+
+            <Bar>
+              <Card
+                  imgSrc="/images/instagram.png"
+                  alt="img"
+                  title="Instagram"
+                  text="Curtidas"
+                  price="A partir de 2$"
+                /> 
+                <Card
+                  imgSrc="/images/instagram.png"
+                  alt="img"
+                  title="Instagram"
+                  text="Curtidas"
+                  price="A partir de 2$"
+                />
+                <Card
+                  imgSrc="/images/instagram.png"
+                  alt="img"
+                  title="Instagram"
+                  text="Curtidas"
+                  price="A partir de 2$"
+                />  
+                <Card
+                  imgSrc="/images/instagram.png"
+                  alt="img"
+                  title="Instagram"
+                  text="Curtidas"
+                  price="A partir de 2$"
+                />
+                <Card
+                  imgSrc="/images/instagram.png"
+                  alt="img"
+                  title="Instagram"
+                  text="Curtidas"
+                  price="A partir de 2$"
+                />    
+            </Bar>
+          </div>     
         </div>
       </main>
     </>
--- src/components/card.tsx
+++ src/components/card.tsx
@@ -1,24 +1,42 @@
+'use client'
 import Image from 'next/image';
+import {useState} from 'react';
 
 type Props = {
-    img: string;
+    imgSrc: string;
     alt: string;
     title: string;
     text: string;
     price: string;
 }
 
-export default function Card({img, alt, title, price}: Props){
+export default function Card({imgSrc, alt, title, price}: Props){
+
+    const [hovered, setHovered] = useState(false);
+
+    const handleMouseEnter = () => {
+      setHovered(true);
+    };
+  
+    const handleMouseLeave = () => {
+      setHovered(false);
+    };
+
     return(
         <>
-            <div className='w-64'>
-                <div>
+            <div className='w-64 p-4 border border-gray-300 shadow-md m-3'>
+                <div className='w-30 flex items-center justify-center pb-8 '>
+                    <div className={`transition-opacity duration-500 ease-in-out ${hovered ? 'opacity-70' : 'opacity-90'}`}
+                        onMouseEnter={handleMouseEnter}
+                        onMouseLeave={handleMouseLeave}
+                        >
                     <Image
-                        src={img}
+                        src={imgSrc}
                         alt={alt}
-                        width={250}
-                        height={250}
+                        width={170}
+                        height={170}
                     ></Image>
+                    </div>
                 </div>
                 <div>
                     <h1>{title}</h1>
